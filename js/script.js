6510f54d95d24bc081b9b4e2302d34bec5b2c48b
Anton <cecon_76@mail.ru>
2023-03-10 16:54:43 +0500

    practise with js

--- js/script.js
+++ js/script.js
@@ -14,6 +14,18 @@
 
 'use strict';
 
+const adv = document.querySelectorAll('.promo__adv img'),
+      poster = document.querySelector('.promo__bg'),
+      movieList = document.querySelector('.promo__interactive-list'),
+      genre = document.querySelector('.promo__genre');  
+
+adv.forEach(item =>{
+    item.remove();
+});
+
+genre.textContent = 'драма';
+poster.style.backgroundImage = 'url("img/bg.jpg")'
+
 const movieDB = {
     movies: [
         "Логан",
@@ -23,4 +35,18 @@ const movieDB = {
         "Скотт Пилигрим против..."
     ]
 };
+
+movieList.innerHTML = '';
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) =>{
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i+1} ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
+
+
 
